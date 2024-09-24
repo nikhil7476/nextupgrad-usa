bfbb66122ae32f8c82ae032978fb950338add138
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-25 05:21:10 +0530

    renamed:    public/assets/logo.webp -> src/assets/logo.webp 	new file:   src/assets/slide.svg 	modified:   src/components/Footer.js 	modified:   src/pages/index.js 	modified:   src/styles/Footer.module.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import brand from "../assets/logo.webp";
 import styles from "@/styles/Footer.module.css";
+import { FaLinkedin } from "react-icons/fa";
 import { Col, Container, Row } from "react-bootstrap";
 
 function Footer() {
@@ -9,7 +13,16 @@ function Footer() {
         <Container>
           <Row>
             <Col className="col-md-3">
-              <h2>Hello</h2>
+              <Link href="/">
+                <Image src={brand} width={"100%"} height={100} />
+              </Link>
+              <div className={styles.inStartups}>
+                <Link href="https://www.linkedin.com/company/nextupgrad-usa" target="_blank">
+                  <p class="text-nowrap">
+                    <FaLinkedin /> Top 20 Indian Startups of 2023
+                  </p>
+                </Link>
+              </div>
             </Col>
             <Col className="col-md-3">
               <h2>Nextupgrad</h2>
