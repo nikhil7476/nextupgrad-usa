e8fbfa8281c050a98d39f3d59cad10c849114747
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-27 20:04:36 +0530

    modified:   src/pages/index.js 	modified:   src/styles/Home.module.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,7 +124,7 @@ function Home() {
               </div>
             </Col>
             <Col className="col-md-6">
-              <Image src={achieve} width={"100%"} height={"auto"} alt="achievements"/>
+              <Image src={achieve} alt="achievements" className={styles.bannerImg}/>
             </Col>
           </Row>
         </Container>
