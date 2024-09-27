bec37b75ec8649e39be869ebe682c751f8c99d9a
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-27 19:36:13 +0530

    new file:   src/assets/achievements.webp 	new file:   src/assets/clutch-logo.svg 	new file:   src/assets/developer-one.webp 	new file:   src/assets/developer-three.webp 	new file:   src/assets/developer-two.webp 	new file:   src/assets/g2-logo.svg 	new file:   src/assets/google-logo.svg 	modified:   src/pages/index.js 	modified:   src/styles/Home.module.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,22 @@
 import Head from "next/head";
+import Link from "next/link";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaArrowRightLong } from "react-icons/fa6";
+import {
+  FaLinkedin,
+  FaGooglePlusSquare,
+  FaStar,
+  FaStarHalf,
+} from "react-icons/fa";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import google from "@/assets/google-logo.svg";
+import clutch from "@/assets/clutch-logo.svg";
+import gtwo from "@/assets/g2-logo.svg";
+import devOne from "@/assets/developer-one.webp";
+import devTwo from "@/assets/developer-two.webp";
+import devThree from "@/assets/developer-three.webp";
+import achieve from "@/assets/achievements.webp";
 
 function Home() {
   return (
@@ -10,10 +28,109 @@ function Home() {
         <link rel="icon" href="/assets/logo.webp" />
       </Head>
       <div>
-        <h1>Home Page !!</h1>
+        <Container>
+          <Row>
+            <Col className="col-md-6">
+              <h1>
+                Hire Software Developers Perfectly Matched by{" "}
+                <span>AI, 10X Faster!</span>
+              </h1>
+              <p>
+                Skip CV screening, interview rounds, &{" "}
+                <strong>save 90% time</strong> by either hiring a software
+                developer or your entire engineering team with us.
+              </p>
+              <div className={styles.bannerBtn}>
+                <Link href="#">
+                  Hire Developers <FaArrowRightLong />
+                </Link>
+                <Link href="#">Contact Us</Link>
+              </div>
+              <div>
+                <div>
+                  <ul>
+                    <li>
+                      <FaStar />
+                    </li>
+                    <li>
+                      <FaStar />
+                    </li>
+                    <li>
+                      <FaStar />
+                    </li>
+                    <li>
+                      <FaStar />
+                    </li>
+                    <li>
+                      <FaStarHalf />
+                    </li>
+                    <li>
+                      <p>20,000+ Reviews</p>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <Image src={google} />
+                    </li>
+                    <li>
+                      <Image src={clutch} />
+                    </li>
+                    <li>
+                      <Image src={gtwo} />
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <ul>
+                    <li>
+                      <Image src={devOne} />
+                    </li>
+                    <li>
+                      <Image src={devTwo} />
+                    </li>
+                    <li>
+                      <Image src={devThree} />
+                    </li>
+                    <li>
+                      <strong>2200+</strong> top Developers
+                      <br />
+                      for your custom needs
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className={styles.inStart}>
+                <div className={styles.inStartups}>
+                  <Link
+                    href="https://www.linkedin.com/company/nextupgrad-usa"
+                    target="_blank"
+                  >
+                    <p class="text-nowrap">
+                      <FaLinkedin /> Top 20 Indian Startups of 2023
+                    </p>
+                  </Link>
+                </div>
+                <div className={styles.inStartups}>
+                  <Link
+                    href="https://www.linkedin.com/company/nextupgrad-usa"
+                    target="_blank"
+                  >
+                    <p class="text-nowrap">
+                      <FaGooglePlusSquare /> Top 20 AI Accelerator Startups of
+                      2023
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </Col>
+            <Col className="col-md-6">
+              <Image src={achieve} width={"100%"} height={"100%"} />
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
 }
 
-export default Home
+export default Home;
