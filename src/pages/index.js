830d0a97d6e0e4ed6a81e0860bd7f3f68270f818
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-29 14:02:01 +0530

    new file:   src/assets/e-commerce.svg 	new file:   src/assets/education.svg 	new file:   src/assets/fast-food.svg 	new file:   src/assets/fmcg.svg 	new file:   src/assets/healthcare.svg 	new file:   src/assets/logistics.svg 	new file:   src/assets/media.svg 	new file:   src/assets/real-estate.svg 	new file:   src/assets/retail.svg 	new file:   src/assets/travel.svg 	modified:   src/pages/index.js 	modified:   src/styles/Home.module.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,18 @@ import devOne from "@/assets/developer-one.webp";
 import devTwo from "@/assets/developer-two.webp";
 import devThree from "@/assets/developer-three.webp";
 import achieve from "@/assets/achievements.webp";
+import ecart from "@/assets/e-commerce.svg";
+import edu from "@/assets/education.svg";
+import food from "@/assets/fast-food.svg";
+import fmcg from "@/assets/fmcg.svg";
+import health from "@/assets/healthcare.svg";
+import logi from "@/assets/logistics.svg";
+import media from "@/assets/media.svg";
+import estate from "@/assets/real-estate.svg";
+import travel from "@/assets/travel.svg";
+import retail from "@/assets/retail.svg";
 
 function Home() {
-
   return (
     <>
       <Head>
@@ -169,21 +178,27 @@ function Home() {
                   <p>
                     <span>
                       <CountUp end={100} duration={5} suffix="+" />
-                    </span><br/>Project Developed
+                    </span>
+                    <br />
+                    Project Developed
                   </p>
                 </Col>
                 <Col>
                   <p>
                     <span>
                       <CountUp end={25} duration={5} suffix="+" />
-                    </span><br/>Industries
+                    </span>
+                    <br />
+                    Industries
                   </p>
                 </Col>
                 <Col>
                   <p>
                     <span>
                       <CountUp end={500} duration={5} suffix="+" />
-                    </span><br/>Website Design
+                    </span>
+                    <br />
+                    Website Design
                   </p>
                 </Col>
               </Row>
@@ -192,22 +207,152 @@ function Home() {
                   <p>
                     <span>
                       <CountUp end={14} duration={5} suffix="+" />
-                    </span><br/>Number of Countries Operations
+                    </span>
+                    <br />
+                    Number of Countries Operations
                   </p>
                 </Col>
                 <Col>
                   <p>
                     <span>
                       <CountUp end={254} duration={5} suffix="+" />
-                    </span><br/>Search Engine Optimisation Projects
+                    </span>
+                    <br />
+                    Search Engine Optimisation Projects
                   </p>
                 </Col>
                 <Col>
                   <p>
                     <span>
                       <CountUp end={139} duration={5} suffix="+" />
-                    </span><br/>Paid Campaign Advertisement & Projects
+                    </span>
+                    <br />
+                    Paid Campaign Advertisement & Projects
                   </p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className={styles.industry}>
+        <Container>
+          <Row>
+            <Col>
+              <h2>
+                <span>Industry</span>
+                <br />
+                Delivering Industry-Focused Software Solutions
+              </h2>
+              <p>
+                Our team of Solution Architect & Software Engineers, Analysts
+                collaborates with clients from diverse industry segments, to
+                understand their roadblocks and objectives, enabling us to
+                develop custom software solutions that are efficient and
+                scalable.
+              </p>
+              <Row className={styles.inds}>
+                <Col>
+                  <Image
+                    src={ecart}
+                    alt="E-Cart"
+                    width={100}
+                    height={100}
+                    title="E-Commerce"
+                  />
+                  <p>E-commerce</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={health}
+                    alt="Heart"
+                    width={100}
+                    height={100}
+                    title="Healthcare"
+                  />
+                  <p>Healthcare</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={edu}
+                    alt="Degree-Cap"
+                    width={100}
+                    height={100}
+                    title="E-Learning"
+                  />
+                  <p>E-Learning</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={media}
+                    alt="Camera"
+                    width={100}
+                    height={100}
+                    title="News & Media"
+                  />
+                  <p>News & Media</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={estate}
+                    alt="Crane"
+                    width={100}
+                    height={100}
+                    title="Real Estate"
+                  />
+                  <p>Real Estate</p>
+                </Col>
+              </Row>
+              <Row className={styles.inds}>
+                <Col>
+                  <Image
+                    src={logi}
+                    alt="Shipment-Truck"
+                    width={100}
+                    height={100}
+                    title="Logistics & Distributions"
+                  />
+                  <p>Logistics & Distributions</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={retail}
+                    alt="Retail"
+                    width={100}
+                    height={100}
+                    title="Retail Shops"
+                  />
+                  <p>Retail Shops</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={food}
+                    alt="Fast-Food"
+                    width={100}
+                    height={100}
+                    title="Food & Beverages"
+                  />
+                  <p>Food & Beverages</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={fmcg}
+                    alt="FMCG"
+                    width={100}
+                    height={100}
+                    title="FMCG"
+                  />
+                  <p>FMCG</p>
+                </Col>
+                <Col>
+                  <Image
+                    src={travel}
+                    alt="Aeroplane"
+                    width={100}
+                    height={100}
+                    title="Hotel & Travel"
+                  />
+                  <p>Hotel & Travel</p>
                 </Col>
               </Row>
             </Col>
