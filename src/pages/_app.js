43e5f7295601290f81d595a78537a41c45c92b3e
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-29 20:11:02 +0530

    modified:   src/pages/_app.js 	modified:   src/pages/_document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,21 +2,11 @@ import "@/styles/globals.css";
 import Header from "@/components/Header";
 import MobileHead from "@/components/MobileHeader";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous"
-        />
-        <link rel="icon" href="/logo.webp" />
-      </Head>
       <Header className="navHdr" />
       <MobileHead className="mobHdr" />
       <main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossOrigin="anonymous"
+        />
+        <link rel="icon" href="/logo.webp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
