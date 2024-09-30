e91bb6b1ed10cdea6516c3332914de356c9339bc
Nikhil Mishra <nikhil.nextupgrad01@gmail.com>
2024-09-30 23:13:31 +0530

    modified:   src/components/MegaMenu.js

--- src/components/MegaMenu.js
+++ src/components/MegaMenu.js
@@ -29,77 +29,85 @@ function MegaMenu() {
                     <div className="container">
                       <div className="row">
                         <div className="col-xs-12 col-sm">
+                          <h3>Organic SEO</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
+                            <li className="list-group-item">SEO Services</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Enterprise SEO Services
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              Digital Marketing Services
+                            </li>
+                            <li className="list-group-item">
+                              Google Local Services Ads Management
+                            </li>
+                            <li className="list-group-item">SEO Audits</li>
+                            <li className="list-group-item">
+                              Generative Engine & Chat Optimization
                             </li>
                           </ul>
                         </div>
                         <div className="col-xs-12 col-sm">
+                          <h3>Digital Advertising</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              PPC Management Services
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              Enterprise PPC Management Services
+                            </li>
+                            <li className="list-group-item">
+                              Social Media Advertising
+                            </li>
+                            <li className="list-group-item">
+                              Enterprise Social Media Advertising
                             </li>
                           </ul>
                         </div>
                         <div className="col-xs-12 col-sm">
+                          <h3>Ecommerce</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Ecommerce SEO Services
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              Ecommerce PPC Services
+                            </li>
+                            <li className="list-group-item">
+                              Ecommerce Social Media Advertising
+                            </li>
+                            <li className="list-group-item">
+                              B2B Ecommerce Enablement
+                            </li>
+                            <li className="list-group-item">
+                              Ecommerce Digital Marketing Services
+                            </li>
+                            <li className="list-group-item">
+                              Ecommerce Marketing Resources
                             </li>
                           </ul>
                         </div>
                         <div className="col-xs-12 col-sm">
+                          <h3>Learn</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
+                            <li className="list-group-item">Our SEO Results</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Our SEO Case Studies
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="col-xs-12 col-sm">
-                          <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
-                            <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              What Is an SEO Company?
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              How to Find the Best SEO Company
+                            </li>
+                            <li className="list-group-item">
+                              SEO Guide for Marketing Managers
+                            </li>
+                            <li className="list-group-item">
+                              What Is Digital Marketing?
+                            </li>
+                            <li className="list-group-item">
+                              Best Digital Marketing Tools
                             </li>
                           </ul>
                         </div>
@@ -115,77 +123,60 @@ function MegaMenu() {
                     <div className="container">
                       <div className="row">
                         <div className="col-xs-12 col-sm">
+                          <h3>Conversion</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Conversion Rate Optimization
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              User Experience Testing
+                            </li>
+                            <li className="list-group-item">
+                              Landing Pages & Funnels
+                            </li>
+                            <li className="list-group-item">
+                              Online Review Acceleration
+                            </li>
+                            <li className="list-group-item">
+                              Website Personalization
                             </li>
                           </ul>
                         </div>
                         <div className="col-xs-12 col-sm">
+                          <h3>Marketing Automation</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Account-Based Marketing Services
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              Lead Nurture Email Marketing
+                            </li>
+                            <li className="list-group-item">
+                              Email Marketing Management
+                            </li>
+                            <li className="list-group-item">
+                              Salesforce Marketing Automation
+                            </li>
+                            <li className="list-group-item">
+                              Microsoft Dynamics Marketing Automation
                             </li>
                           </ul>
                         </div>
                         <div className="col-xs-12 col-sm">
+                          <h3>E-commerce Platefom</h3>
                           <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
+                            <li className="list-group-item">SEO for Amazon</li>
                             <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Advertising Management for Amazon
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="col-xs-12 col-sm">
-                          <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
-                            <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Facebook Marketplace for Business
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
-                            </li>
-                          </ul>
-                        </div>
-                        <div className="col-xs-12 col-sm">
-                          <ul className="list-group">
-                            <li className="list-group-item">Cras justo odio</li>
-                            <li className="list-group-item">
-                              Dapibus ac facilisis in
-                            </li>
-                            <li className="list-group-item">Morbi leo risus</li>
-                            <li className="list-group-item">
-                              Porta ac consectetur ac
+                              Influencer Marketing Services
                             </li>
                             <li className="list-group-item">
-                              Vestibulum at eros
+                              Walmart Marketplace Advertising
                             </li>
                           </ul>
                         </div>
