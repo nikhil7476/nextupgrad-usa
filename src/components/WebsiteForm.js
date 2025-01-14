6eecab08e1ff4e23d4b552d83634f3742446db9f
Nikhil Mishra <nikhil9027917476@gmail.com>
2025-01-14 20:58:06 +0530

    modified:   src/components/WebsiteForm.js

--- src/components/WebsiteForm.js
+++ src/components/WebsiteForm.js
@@ -14,7 +14,7 @@ function TestForms() {
     <>
       <Form className={styles.proposalForm} onSubmit={handleSubmit}>
         <Form.Group className={styles.propEmail} controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Enter your website" />
+          <Form.Control type="text" placeholder="Enter your website" required />
         </Form.Group>
         <Button variant="primary" type="submit" className={styles.ctaBtn}>
           Send Me A Proposal
